7fa40a66fc2fdb19df502fbc56fa3f8c10f819b0
Jinhyun Kim <jinkim608@gmail.com>
2014-10-21 22:59:37 -0400

    Initial commit of Google Maps API demo - After replacing Maps API Key on manifest, can bring up maps with data correctly loading

---
+++ app/src/main/java/dev/jinkim/googlemapsdemo/MapsActivity.java
@@ -0,0 +1,18 @@
+package dev.jinkim.googlemapsdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Jin on 10/21/14.
+ */
+public class MapsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_maps_demo);
+    }
+
+}
